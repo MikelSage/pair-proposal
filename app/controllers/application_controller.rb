44006a9e1c935ec1421fcd884be820bb8c4d0d01
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-23 14:13:35 -0600

    prevent pupils from other pupils

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :current_admin, :not_current_user
+  helper_method :current_user, :current_admin, :not_current_user, :require_pupil_to_tutor
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,7 +11,22 @@ class ApplicationController < ActionController::Base
   end
 
   def not_current_user
-    # require "pry"; binding.pry
     current_user.id != params[:id].to_i
+  end
+
+  def pupil_to_pupil
+    user = User.find(params[:id])
+    current_user.pupil? && user.pupil?
+  end
+
+  def tutor_to_pupil
+    user = User.find(params[:id])
+    current_user.tutor? && user.pupil?
+  end
+
+  def require_pupil_to_tutor
+    if not_current_user && (pupil_to_pupil || tutor_to_pupil)
+      render file: 'public/404'
+    end
   end
 end
