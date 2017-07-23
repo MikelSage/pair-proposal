c71cf194d23daa4b1d4ec135897e7855b7a41490
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-23 14:33:50 -0600

    require current user only on edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_pupil_to_tutor, only: [:show, :edit]
-  before_action :require_current_user, only: [:show, :edit]
+  before_action :require_current_user, only:  [:edit]
   def new
     @user = User.new
   end
